d30d6ef8afa2c9af72343a1e2801d478de778960
rigifdaga <rfdauliaa@gmail.com>
2023-10-12 17:57:01 +0700

    fix settings.py

--- amoron/settings.py
+++ amoron/settings.py
@@ -138,3 +138,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
